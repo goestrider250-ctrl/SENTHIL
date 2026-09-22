b4a92cd50cfe9eced9e134a0fae7da4ff9b7a029
goestrider250-ctrl <senthilkumaran2995@gmail.com>
2026-09-23 00:56:55 +0530

    update

---
+++ JavaScript/Day1/assets/index.js
@@ -0,0 +1,15 @@
+console.log("senthil");
+console.log(20);
+console.log("web developer");
+console.log("SLA STUDEND");
+console.log("The age is ");
+alert("incorrect")
+
+
+
+
+
+
+
+console.error("error");
+console.warn("waring");

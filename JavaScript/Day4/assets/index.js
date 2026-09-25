9f6c10632d02507439ccc1762cb0ef724af2fb8f
goestrider250-ctrl <senthilkumaran2995@gmail.com>
2026-09-25 12:59:47 +0530

    update

---
+++ JavaScript/Day4/assets/index.js
@@ -0,0 +1,20 @@
+let money = 100;
+
+if (money >= 40) {
+    console.log("You can buy an ice cream");
+}
+
+if (money >= 30) {
+    console.log("You can buy an  banana");
+}
+
+
+if (money >= 20) {
+    console.log("You can buy an  choklate");
+}
+
+
+if (money >= 10) {
+    console.log("You can buy an bingo");
+}
+else (balance=0)
